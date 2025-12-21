948fb905fea89a108c7942d62c3650a22ec1e514
TranPhucKhai-22130109 <22130109@st.hcmuaf.edu.vn>
2025-12-22 00:11:17 +0700

    tạo cart bằng redux toolkit

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 // Reuse component
 const NavList = styled.ul`
@@ -8,7 +9,19 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
+const CartBadge = styled.span`
+  background: red;
+  color: white;
+  border-radius: 50%;
+  padding: 2px 6px;
+  font-size: 12px;
+  margin-left: 6px;
+`;
+
 function Navbar() {
+    const cartCount = useSelector((state) =>
+        state.cart.list.reduce((sum, item) => sum + item.quantity, 0)
+    );
   return (
     <nav>
       <NavList>
@@ -23,6 +36,15 @@ function Navbar() {
             <span>Product</span>
           </NavLink>
         </li>
+
+          <li>
+              <NavLink to="/cart">
+            <span>
+              Cart
+                {cartCount > 0 && <CartBadge>{cartCount}</CartBadge>}
+            </span>
+              </NavLink>
+          </li>
       </NavList>
     </nav>
   );
