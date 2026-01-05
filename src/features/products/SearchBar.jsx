17dfe0563d9d01c91f03fef8f53c1452ca88ee90
tungdo <sont4036@gmail.com>
2026-01-05 19:50:54 +0700

    sua giao dien

--- src/features/products/SearchBar.jsx
+++ src/features/products/SearchBar.jsx
@@ -4,9 +4,10 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { CiFilter } from "react-icons/ci";
 
 import { useCategories } from "./useCategories";
+import { GrPowerReset } from "react-icons/gr";
 function SearchBar() {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const [value, setValue] = useState(searchParams.get("category") || "");
   const [sortVal, setSortVal] = useState(searchParams.get("sort") || "");
@@ -47,54 +48,73 @@ function SearchBar() {
     navigate(`/product?${params.toString()}`);
   }
 
-  return (
-    <div className="flex items-center gap-x-3 mb-3">
-      {/* Category */}
-      <div className="flex items-center gap-2">
-        <label>Thể loại: </label>
-        <select
-          value={value}
-          onChange={(e) => handleChangeSelect(e)}
-          name="tag"
-          className="px-8 py-1 border border-solid rounded-sm text-sm w-[50%]"
-        >
-          <option value="">Tất cả</option>
-          {item.map((cat) => (
-            <option key={cat.id} value={cat.id}>
-              {cat.name}
-            </option>
-          ))}
-        </select>
-      </div>
+  // Reset
+  function resetAllSort() {
+    setSortVal("");
+    setValue("");
+    // item = "";
 
-      {/* Sort */}
-      <div className="flex items-center gap-2 ">
-        <div>
-          <label>Sắp xếp: </label>
+    setSearchParams({});
+  }
+
+  return (
+    <>
+      <div className="flex items-center gap-x-3 mb-3">
+        {/* Category */}
+        <div className="flex items-center gap-2">
+          <label>Thể loại: </label>
           <select
-            value={sortVal}
-            onChange={(e) => handleChangeSort(e)}
-            className="px-8 py-1 border border-solid rounded-sm"
-            name="sort"
+            value={value}
+            onChange={(e) => handleChangeSelect(e)}
+            name="tag"
+            className="px-8 py-1 border border-solid rounded-sm text-sm w-[50%]"
           >
-            <option value="">Mặc định</option>
-            <option value="price-asc">Giá thấp - cao</option>
-            <option value="price-desc">Giá cao - thấp</option>
-            <option value="name-asc">Tên từ A - Z</option>
-            <option value="name-desc">Tên từ Z - A</option>
+            <option value="">Tất cả</option>
+            {item.map((cat) => (
+              <option key={cat.id} value={cat.id}>
+                {cat.name}
+              </option>
+            ))}
           </select>
         </div>
-        <div className="ml-4">
-          <button
-            onClick={handleSubmit}
-            className={`p-1 w-20 border border-solid rounded-sm bg-blue-500 text-white flex items-center justify-around cursor-pointer`}
-          >
-            <CiFilter />
-            Lọc
-          </button>
+
+        {/* Sort */}
+        <div className="flex items-center gap-2 ">
+          <div>
+            <label>Sắp xếp: </label>
+            <select
+              value={sortVal}
+              onChange={(e) => handleChangeSort(e)}
+              className="px-8 py-1 border border-solid rounded-sm"
+              name="sort"
+            >
+              <option value="">Mặc định</option>
+              <option value="price-asc">Giá thấp - cao</option>
+              <option value="price-desc">Giá cao - thấp</option>
+              <option value="name-asc">Tên từ A - Z</option>
+              <option value="name-desc">Tên từ Z - A</option>
+            </select>
+          </div>
+          <div className="ml-4">
+            <button
+              onClick={handleSubmit}
+              className={`p-1 w-20 border border-solid rounded-sm bg-blue-500 text-white flex items-center justify-around cursor-pointer`}
+            >
+              <CiFilter />
+              Lọc
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* Reset */}
+      <span className="flex items-center gap-x-3 text-red-500 cursor-pointer">
+        <GrPowerReset className="text-xl" />
+        <p onClick={resetAllSort} className="text-sm hover:underline">
+          Khôi phục bộ lọc
+        </p>
+      </span>
+    </>
   );
 }
 export default SearchBar;
