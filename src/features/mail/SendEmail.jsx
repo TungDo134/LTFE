6bfbfe28dcee130ba3036e314af49d79da571fa1
tungdo <sont4036@gmail.com>
2026-01-06 17:59:24 +0700

    sua logic gui mail tao order + quen mk, sua logic checkout

--- src/features/mail/SendEmail.jsx
+++ src/features/mail/SendEmail.jsx
@@ -99,7 +99,7 @@ function SendMail() {
     try {
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        "template_wfr5civ",
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         templateParams,
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       );
