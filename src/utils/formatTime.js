c50a61cf780943d4411bb1a7fb9ba4d824bfa5b0
Xuantrung1902 <22130304@st.hcmuaf.edu.vn>
2026-01-06 17:59:06 +0700

    new message notification

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -4,3 +4,12 @@ export const formatTime = (seconds) => {
     return `${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 };
 
+export const formatDate = (isoString) => {
+    const date = new Date(isoString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // tháng trong JS từ 0-11
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+};
+
